c4b9686b975aee0efba099e42b815e312e312c27
mikemerin <mike.d.merin@gmail.com>
2020-05-19 13:43:53 -0400

    load/save working

--- bundles/dashboard/dashboard/functions.js
+++ bundles/dashboard/dashboard/functions.js
@@ -11,7 +11,7 @@ class DashboardForm {
   generateForm() {
     this.fields.forEach(({
       fieldName,
-      type,
+      inputType,
       min,
       max,
       options,
@@ -23,14 +23,14 @@ class DashboardForm {
       var label = $("<label>", { text: fieldName });
       var brClear = $("<br>", { clear: "all" });
 
-      switch(type) {
+      switch(inputType) {
         case "text":
         case "number":
-          input = createTextBox(inputId, type, placeholder);
+          input = createTextBox(inputId, inputType, placeholder);
           break;
         case "radio":
         case "checkbox":
-          input = createSelectGroup(inputId, type, "", options, orientation);
+          input = createSelectGroup(inputId, inputType, "", options, orientation);
           break;
         case "dropdown":
           input = createDropdown(inputId, "", options);
@@ -41,37 +41,61 @@ class DashboardForm {
       div.append( label, "<br>", input, brClear );
       $("#" + this.fieldGroup + "Fields").append(div, "<br>");
 
-      this.addFieldAction(inputId, type);
-      this.attachReplicant(inputId);
+      this.addFieldAction(inputId, inputType);
+      this.addReplicantValue(inputId);
       this.addSubmitAction(inputId);
     })
   };
 
-  addFieldAction(inputId, type) {
+  addFieldAction(inputId, inputType) {
     this.fieldActions[inputId] = {
       field: $("#" + inputId),
-      type: type,
+      inputType: inputType,
       replicant: nodecg.Replicant(inputId)
     }
     // console.log("field", "#" + inputId + "Field", this.fieldActions[inputId].field);
   };
 
-  attachReplicant(inputId) {
-    const { field, replicant } = this.fieldActions[inputId];
-    console.log(this.fieldActions[inputId]);
+  addReplicantValue(inputId) {
+    const { field, inputType, replicant } = this.fieldActions[inputId];
     replicant.on('change', (newValue, oldValue) => {
-      console.log(newValue, oldValue)
-      field.val(newValue);
+      // console.log("t.fieldActions[iid], field, replicant", this.fieldActions[inputId], field, replicant);
+      // console.log("new val:", newValue, "old val:", oldValue)
+      switch(inputType) {
+        case "text":
+          field.val(newValue);
+          break;
+        case "radio":
+        case "checkbox":
+          var choices = newValue.split("; ");
+          $("input[name$='" + inputId + "']").each((i,x) => {
+            if (choices.includes(x.value)) $(x).prop("checked", true);
+          })
+          break;
+        default: ""; break;
+      }
     });
   };
 
   addSubmitAction(inputId) {
     $("#" + this.fieldGroup + "SaveButton")[0].addEventListener('click', () => {
-      const { field, replicant } = this.fieldActions[inputId];
-      console.log(field, replicant)
-      console.log("before", replicant.value)
-      replicant.value = field.val();
-      console.log("after", field.val(), replicant.value)
+      const { field, inputType, replicant } = this.fieldActions[inputId];
+      // console.log(field, inputType, replicant, "input: ", replicant.value)
+      var output = "";
+      switch(inputType) {
+        case "text":
+          output = field.val();
+          break;
+        case "radio":
+        case "checkbox":
+          var choices = [];
+          $("input[name$='" + inputId + "']").filter((i,input) => $(input).is(":checked")).each((i,x) => choices.push(x.value));
+          output = choices.join("; ");
+          break;
+        default: ""; break;
+      }
+      replicant.value = output
+      // console.log("after", output, "=>", replicant.value)
     })
   };
 
@@ -79,21 +103,21 @@ class DashboardForm {
 
 // Helpers, move into class or have an external helper class
 
-function createTextBox(inputId, type, placeholder) {
+function createTextBox(inputId, inputType, placeholder) {
   return $("<input>", {
     id: inputId,
-    type: type,
+    inputType: inputType,
     placeholder: placeholder || ""
   });
 }
 
-function createSelectGroup(inputId, type, selected, options, orientation) {
-  var group = $("<div class='" + type + "-group " + orientation + "'>", { id: inputId + "Group" });
+function createSelectGroup(inputId, inputType, selected, options, orientation) {
+  var group = $("<div class='" + inputType + "-group " + orientation + "'>", { id: inputId + "Group" });
   options.forEach((text, i) => {
     // console.log(text)
     var id = sanitize(text);
-    var radio = $("<input>", {
-      type: type,
+    var select = $("<input>", {
+      type: inputType,
       id: id,
       name: inputId,
       value: text,
@@ -103,7 +127,7 @@ function createSelectGroup(inputId, type, selected, options, orientation) {
       for: id,
       text: text
     });
-    group.append(radio, label);
+    group.append(select, label);
   });
   return group;
 }
