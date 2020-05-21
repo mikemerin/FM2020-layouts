54287eab5ef222fcced3f4117b56b768ab564c8c
Mike Merin <mikemerinweather@gmail.com>
2020-05-21 16:47:40 -0400

    pre-rep overhaul

--- bundles/dashboard/graphics/js/functions.js
+++ bundles/dashboard/graphics/js/functions.js
@@ -6,7 +6,21 @@ class Layout {
 
   getLayoutName = () => {
     // this.name = location.href.match(/(layout.*)(.html)/)[1];
-    debugger
+    // var replicants = [];
+    // ["mainInfo", "playerInfo"].forEach(fieldGroup => {
+    //     fieldGroups[fieldGroup].fields.forEach(field => {
+    //         var fieldSanitized = sanitize(field.fieldName);
+    //         replicants.push(nodecg.Replicant(fieldGroup + "_field_" + fieldSanitized);
+    //     })
+    // })
+    // console.log(Object.keys(replicants))
+    // console.log(replicants)
+    // NodeCG.waitForReplicants.apply(Object.keys(replicants)).then(() => {
+    //     console.log("hit")
+    //     Object.keys(replicants).forEach(replicant => {
+    //         console.log(replicants[replicant])
+    //     })
+    // })
     // pick up with fieldGroups, getting all the replicants. Start with getting the layout name (layout_1_608), then get the other fields, then make a stylesheet for the layout against the locations
   }
 
