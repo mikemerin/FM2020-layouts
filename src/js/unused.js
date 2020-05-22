b7ddbe5baf7eedef48da402bc4bbc857f726eb24
mikemerin <mike.d.merin@gmail.com>
2020-05-22 12:58:28 -0400

    overhaul of dashboard class and replicant functions

---
+++ src/js/unused.js
@@ -0,0 +1,15 @@
+
+
+  // part of DashboardForm class. not using, css overlaps are causing more of a headache
+  // inside DashboardForm:
+  // addRadioUncheckListeners() {
+  //   $("input:radio").on("click", function(e) {
+  //       var button = $(this);
+  //       if (button.is(".radioCheck")) {
+  //           button.prop("checked", false).removeClass("radioCheck");
+  //       } else {
+  //           $("buttonut:radio[name='" + button.prop("name") + "'].radioCheck").removeClass("radioCheck");
+  //           button.addClass("radioCheck");
+  //       }
+  //   });
+  // }
