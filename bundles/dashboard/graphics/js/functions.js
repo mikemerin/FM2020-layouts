df65486dc146e980e508460a156de2428b2a2a69
Mike Merin <mikemerinweather@gmail.com>
2020-05-27 20:24:59 -0400

    starting the layout generation

--- bundles/dashboard/graphics/js/functions.js
+++ bundles/dashboard/graphics/js/functions.js
@@ -1,27 +1,58 @@
 class Layout {
 
   constructor() {
-    this.name = this.getLayoutName()
+    this.replicant;
+    this.replicantValues;
+    this.url = "";
+    // this.start();
   }
 
-  getLayoutName = () => {
-    // this.name = location.href.match(/(layout.*)(.html)/)[1];
-    // var replicants = [];
+  start = () => {
+    this.replicant = nodecg.Replicant("fieldValues");
+    const { name, namespace } = this.replicant;
+
+    nodecg.readReplicant(name, namespace, replicantValues => {
+      this.replicantValues = replicantValues;
+      this.setLayoutName();
+      this.setBaseImage();
+    })
+  }
+
+  setLayoutName = () => {
+    const players = this.replicantValues.playerInfo.numberOfPlayers;
+    const resolution = this.replicantValues.mainInfo.resolution;
+    this.url = "/assets/dashboard/baseLayouts" + players + "P/base" + resolution + ".png";
+    // var asset = nodecg.Replicant("assets:baseLayouts" + players + "P");
+    // const { name, namespace } = asset;
+    //
+    // nodecg.readReplicant(name, namespace, replicantValues => {
+    //   console.log(name, namespace, replicantValues)
+    // })
+  }
+
+  setBaseImage = () => {
+    const baseImage = $("<img>", {
+      src: this.url
+    })
+    console.log(this)
+    console.log(baseImage)
+    $("body").append(baseImage);
+  }
+
+  setFields = () => {
     // ["mainInfo", "playerInfo"].forEach(fieldGroup => {
     //     fieldGroups[fieldGroup].fields.forEach(field => {
-    //         var fieldSanitized = sanitize(field.fieldName);
-    //         replicants.push(nodecg.Replicant(fieldGroup + "_field_" + fieldSanitized);
+    //       // console.log(field)
+    //       var fieldSanitized = sanitize(field.fieldName);
+    //       console.log(fieldSanitized)
+    //       var replicant = nodecg.Replicant(fieldGroup + "_field_" + fieldSanitized);
+    //       this.fields[fieldSanitized] = {
+    //         replicant: replicant
+    //       }
+    //       this.replicants.push(replicant);
     //     })
     // })
-    // console.log(Object.keys(replicants))
-    // console.log(replicants)
-    // NodeCG.waitForReplicants.apply(Object.keys(replicants)).then(() => {
-    //     console.log("hit")
-    //     Object.keys(replicants).forEach(replicant => {
-    //         console.log(replicants[replicant])
-    //     })
-    // })
-    // pick up with fieldGroups, getting all the replicants. Start with getting the layout name (layout_1_608), then get the other fields, then make a stylesheet for the layout against the locations
+    // console.log(this.fields)
   }
 
 }
