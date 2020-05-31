97efeeabd23c1488ba9e43d63ab20f5c06e32583
mikemerin <mike.d.merin@gmail.com>
2020-05-30 20:52:25 -0400

    deepMerge working for multi-resolutions

--- src/js/globalFunctions.js
+++ src/js/globalFunctions.js
@@ -8,6 +8,29 @@ const sanitize = (str) => {
   return str.replace(/\s(\w)/g, ($1) => $1[1].toUpperCase());
 }
 
+function deepMerge(target, source) {
+  const isObject = (obj) => obj && typeof obj === 'object';
+
+  if (!isObject(target) || !isObject(source)) {
+    return source;
+  }
+
+  Object.keys(source).forEach(key => {
+    const targetValue = target[key];
+    const sourceValue = source[key];
+
+    if (Array.isArray(targetValue) && Array.isArray(sourceValue)) {
+      target[key] = targetValue.concat(sourceValue);
+    } else if (isObject(targetValue) && isObject(sourceValue)) {
+      target[key] = deepMerge(Object.assign({}, targetValue), sourceValue);
+    } else {
+      target[key] = sourceValue;
+    }
+  });
+
+  return target;
+}
+
 // class GlobalFunctions {
 //
 //   sanitize = (str) => {
