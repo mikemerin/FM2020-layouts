933c135e06876781f6da71ae4cdc20b8fe9de105
mikemerin <mike.d.merin@gmail.com>
2020-06-14 23:34:19 -0400

    BG opacity field, min/max/step options for numbers, separate admin JS

---
+++ bundles/dashboard/dashboard/js/adminPanel.js
@@ -0,0 +1,59 @@
+class AdminPanel {
+
+  setRunSearch = () => {
+
+  };
+
+  setFieldsInfo = () => {
+
+  };
+
+  setLoadLayoutInfo = () => {
+    const text = "Layout window for";
+    const newId = "Open New Window";
+
+    $("#loadLayout").append( //todo: next
+      $("<div>", {
+        id: sanitize(text),
+        class: "loadButton",
+        text: text
+      })
+    );
+
+    $("#loadLayout").append( //todo: next
+      $("<button>", {
+        id: sanitize(newId) + "Window",
+        class: "loadButton",
+        text: newId
+      })
+    );
+
+    const replicant = nodecg.Replicant("fieldValues");
+
+    replicant.on("change", (newValue, oldValue) => {
+      const loadButton = $("#" + sanitize(newId) + "Window"); //todo: next
+      loadButton.off();
+
+      var labelText = "Cannot load layout until more info is chosen";
+
+      if (newValue && newValue["playerInfo"] && newValue["gameInfo"] ) {
+        const numberOfPlayers = newValue["playerInfo"]["numberOfPlayers"];
+        const { resolution, gameNameTitle } = newValue["gameInfo"];
+        labelText = text + "<br>" + numberOfPlayers + "P " + resolution + " - " + gameNameTitle;
+
+        loadButton.on("click", (e) => {
+          e.preventDefault();
+          if (!!numberOfPlayers && numberOfPlayers !== "N/A" && !!resolution && resolution !== "N/A") {
+            var url = "http://localhost:9090/bundles/dashboard/graphics/layout.html";
+            window.open(url);
+          };
+        });
+      } else {
+        loadButton.off();
+      }
+
+      $("#" + sanitize(text)).html(labelText); //todo: next
+    });
+  };
+
+};
